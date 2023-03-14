8d5c1c0a6bc051d8f800015aea4d2e4517c02e0d
mesutAygun1881 <80922155+mesutAygun1881@users.noreply.github.com>
2023-03-14 23:33:39 +0300

    Initial Commit

---
+++ PhotoPicker/PhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerApp.swift
+//  PhotoPicker
+//
+//  Created by Mesut Aygün on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
